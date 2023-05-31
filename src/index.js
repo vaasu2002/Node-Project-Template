e4cd40610486e248137732465d961ec00a1606fd
vaasu2002 <vaasu.bisht2021@vitbhopal.ac.in>
2023-05-31 11:11:19 +0530

    index file

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const { ServerConfig } = require('./config');
+const { Logger } = require('./config');
+const apiRoutes = require('./routes');
+
+const app = express();
+
+app.use('/api', apiRoutes);
+
+app.listen(ServerConfig.PORT, () => {
+    console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
+    Logger.info(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
+});
