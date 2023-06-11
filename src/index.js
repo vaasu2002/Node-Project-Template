ce7ea548cf3c37826390b78a01112cb33606edc8
Vaasu Bisht <92209437+vaasu2002@users.noreply.github.com>
2023-06-11 22:02:28 +0530

    Update index.js
    added urlencoded and.json({}) middleware

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ const apiRoutes = require('./routes');
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 app.use('/api', apiRoutes);
 
 app.listen(ServerConfig.PORT, () => {
